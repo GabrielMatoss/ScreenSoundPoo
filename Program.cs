ead9a3da8645e47797156636854e2c1038abb18d
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-03-16 09:14:12 -0300

    (feat): utilizando função de exibir as musicas do album, através da composição

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Brothers of Metal";
-musica1.Artista = "Manowar";
+﻿Album albumDoManowar = new Album();
+albumDoManowar.Nome = "Louder than Hell";
 
+Musica musica1 = new Musica();
+musica1.Nome = "Brothers of Metal (Pt. 1)";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
-musica2.Nome = "The End";
-musica2.Artista = "Linkin Park";
+musica2.Nome = "Number 1";
+musica2.Duracao = 307;
 
-musica1.ExibirFichaTecnica();
-Console.WriteLine(musica1.DescricaoResumida);
+albumDoManowar.AdicionarMusica(musica1);
+albumDoManowar.AdicionarMusica(musica2);
+
+albumDoManowar.ExibirMusicasDoAlbum();
